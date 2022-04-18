948eb91d626e2a0b6d6ed265d22ce18667493390
iseneld <84731866+iseneld@users.noreply.github.com>
2022-04-18 22:25:43 +0200

    Fantastic exercise.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import { StarRating } from "./Exercises/StarRating";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  return <StarRating totalStars={10} />;
+};
 
 export default App;
---
+++ src/Exercises/StarRating.jsx
@@ -0,0 +1,36 @@
+
+// Exercise made in: https://www.linkedin.com/learning-login/share?account=37488348&forceAccount=false&redirect=https%3A%2F%2Fwww.linkedin.com%2Flearning%2Freact-hooks%2Fsending-interactions-up-component-trees%3Ftrk%3Dshare_video_url%26shareId%3DV57MAj0pSaGnmnNb7obt7g%253D%253D
+
+import { useState } from 'react';
+import { FaStar } from 'react-icons/fa'
+
+// Creates an empty array that is {length} long.
+const createArray = (length) => [...Array(length)];
+
+// Creates a red star if true or gray star if false.
+// Clicking a star runs the onSelect fn.
+const Star = ( {selected = false, onSelect} ) => (
+  <FaStar 
+    color={selected ? "red" : "gray"}
+    onClick={onSelect} />
+);
+
+// Creates row of stars thats {totalStars} long.
+export const StarRating = ({totalStars = 5}) => { 
+  const [selectedStars, setSelectedStars] = useState(0);
+
+  return ( 
+    <>
+      { createArray(totalStars).map((n, i) => 
+      <Star
+        key={i} 
+        // Sends true if selectedStars is greater than or equal to i.
+        selected={selectedStars > i} 
+        // Sets selectedStars to a value equals to the clicked star's position.
+        onSelect={() => setSelectedStars(i + 1)} /> )}
+
+        <p>{selectedStars} of {totalStars}</p>
+    </>
+    
+  );
+}
