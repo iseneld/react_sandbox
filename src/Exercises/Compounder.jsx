94716ffab221786f61fd60a60e690c1379b9c1d8
iseneld <84731866+iseneld@users.noreply.github.com>
2022-04-20 18:31:43 +0200

    Compunder in the worx. React Hook exercise.

--- src/Exercises/Compounder.jsx
+++ src/Exercises/Compounder.jsx
@@ -1,7 +1,40 @@
+import { useState, useEffect } from "react";
+
 export const Compounder = () => {
+  const [age, setAge] = useState(1);
+  const [monthlySavings, setMonthlySavings] = useState(1000);
+  const [annualReturn, setAnnualReturn] = useState(1);
+  const [bajs, setBajs] = useState(1);
+
+  useEffect(() => {
+      for (var i = 0; i < age; i++) {
+        setBajs(bajs);
+      }
+  }, [bajs, age]);
+
   return (
     <main>
-      "Compounder"
+      <label>
+        Age: 
+        <input type="range" min="18" max="65" className="slider" id="myRange" 
+          onChange={(e) => {setAge(e.target.value)}}/>
+        {age} years old
+      </label>
+      <label>
+        Monthly savings: 
+        <input type="range" min="1000" max="9999" className="slider" id="myRange" 
+          onChange={(e) => {setMonthlySavings(e.target.value)}}/>
+        {monthlySavings} SEK/month
+      </label>
+      <label>
+        Annual return: 
+        <input type="range" min="0" max="15" className="slider" id="myRange" 
+          onChange={(e) => {setAnnualReturn(e.target.value)}}/>
+        {annualReturn}%
+      </label>
+      
+      <p>{bajs} kr</p>
+      
     </main>
   );
 }
