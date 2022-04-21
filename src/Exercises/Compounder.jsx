09aae04d63d281036c15b435bd529c3b2d48b8a6
iseneld <84731866+iseneld@users.noreply.github.com>
2022-04-21 09:50:33 +0200

    Compound calculation now works!

--- src/Exercises/Compounder.jsx
+++ src/Exercises/Compounder.jsx
@@ -1,16 +1,21 @@
 import { useState, useEffect } from "react";
 
 export const Compounder = () => {
-  const [age, setAge] = useState(1);
-  const [monthlySavings, setMonthlySavings] = useState(1000);
-  const [annualReturn, setAnnualReturn] = useState(1);
-  const [bajs, setBajs] = useState(1);
+  const [age, setAge] = useState(33);
+  const [savings, setSavings] = useState(1000);
+  const [returns, setReturns] = useState(1.04);
+  const [compound, setCompound] = useState(1);
 
   useEffect(() => {
-      for (var i = 0; i < age; i++) {
-        setBajs(bajs);
-      }
-  }, [bajs, age]);
+      let prutt = Math.pow(returns, (65-age));
+      setCompound(prutt);
+    
+  }, [compound, returns, age]);
+
+  // useEffect(() => {
+  //   let prutt = Math.pow(returns, (65-age));
+  //   setCompound(prutt);
+  // }, [compound, returns, age]);
 
   return (
     <main>
@@ -23,17 +28,19 @@ export const Compounder = () => {
       <label>
         Monthly savings: 
         <input type="range" min="1000" max="9999" className="slider" id="myRange" 
-          onChange={(e) => {setMonthlySavings(e.target.value)}}/>
-        {monthlySavings} SEK/month
+          onChange={(e) => {setSavings(e.target.value)}}/>
+        {savings} SEK/month
       </label>
       <label>
         Annual return: 
-        <input type="range" min="0" max="15" className="slider" id="myRange" 
-          onChange={(e) => {setAnnualReturn(e.target.value)}}/>
-        {annualReturn}%
+        <input type="range" min="0" max="8" className="slider" id="myRange" 
+          onChange={(e) => {setReturns((e.target.value/100)+1)}} />
+        {((returns*100)-100).toFixed(0)}%
       </label>
       
-      <p>{bajs} kr</p>
+      <h1>{((((savings*12)*(40-age))*compound)/1000000).toFixed(2)} MKr at 40</h1>
+      <h1>{((((savings*12)*(50-age))*compound)/1000000).toFixed(2)} MKr at 50</h1>
+      <h1>{((((savings*12)*(65-age))*compound)/1000000).toFixed(2)} MKr at 65</h1>
       
     </main>
   );
