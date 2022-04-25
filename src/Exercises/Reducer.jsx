3660c2d7edd779f14f43395a416232072a1c435e
iseneld <84731866+iseneld@users.noreply.github.com>
2022-04-25 15:16:44 +0200

    Redcuer experiment progress.

--- src/Exercises/Reducer.jsx
+++ src/Exercises/Reducer.jsx
@@ -3,13 +3,13 @@ import { useReducer } from 'react';
 
 export const Reducer = () => { 
   const [number, setNumber] = useReducer(
-    (number, newNumber) => number + newNumber, 
+    (number, newValue) => number * newValue, 
     0
   );
 
   return ( 
     <main>
-      <h1 onClick={() => setNumber(1)}>{number}</h1>
+      <h1 onClick={() => setNumber(2)}>{number}</h1>
     </main> 
   );
 }
